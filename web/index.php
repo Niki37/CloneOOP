10d1b9d310ae4044116d38deea04fda35c4638bb
Vidnov_N <scrappy1994@mail.ru>
2016-09-26 13:40:34 +0300

    Lesson 1.1

--- web/index.php
+++ web/index.php
@@ -1,46 +1,15 @@
 <?php
-/**
- * Это единая точка вхоsда для нашего приложения.
- * На этот файл будут переадресованы все запросы нашего сайта.
- */
 
-// Подключаем файл, где храниться автозагрузчик классов
-require __DIR__ . '/../app/Loader.php';
+class Room
+{
+	public $color = 'red';
+	public function getColor()
+	{
+		echo $this->color;
+	}
 
-// создаем экземпляр класса автозагрузчика
-$loader = new \app\Loader(); 
+}
+$object= new Room();
 
-// добавляем соответствующие нэймспэйсу директории
-$loader->addNamespace('app', realpath(__DIR__ . '/../app'));
-$loader->addNamespace('liw\\core',  realpath(__DIR__ . '/../vendor/liw/core'));
-$loader->addNamespace('liw\\contracts',  realpath(__DIR__ . '/../vendor/liw/interfaces'));
-
-// регистрируем автозагрузчик
-$loader->register();
-
-// регистрируем свой обработчик ошибок и исключений.
-(new \liw\core\ErrorHandler)->register();
-
-$var = 'Переменная из глобальной области видимости';
-
-// Простой пример замыкания.
-// В качестве аргумента передали переменную из текущей области видимости
-$closure1 = function() use ($var) {
-    echo $var . '<br>';
-};
-
-// именно эта строка выполнит тело замыкания1
-$closure1();
-
-// передаем параметр замыканию вручную
-$closure2 = function($var) {
-    // здесь выведется переменная из локальной области видимости, переданная явно
-    echo $var;
-};
-
-// именно это страка выполнит тело замыкания2
-$closure2('Вручную переданный параметр');
-
-
-
-
+$object->getColor();
+?>
